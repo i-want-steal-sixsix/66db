9f5ccddf5a3cf594d9fb876162531b660851560b
UnicISE <ravenling@qq.com>
2023-01-05 16:49:12 +0800

    sql parser commit

---
+++ src/sqlparser/ast.cpp
@@ -0,0 +1,7 @@
+#include "ast.h"
+
+namespace ast {
+
+    std::shared_ptr<ASTNode> parse_tree;
+
+}
---
+++ src/sqlparser/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdio.h>
+#include <memory>
+#include "ast.h"
+#include "parsor_bison.tab.h"
+#include "lex.yy.h"
+
+int main(int argc, char** argv){
+    while (1) {
+        std::cout << "input> ";
+        char *line_read = new char[110];
+        std::cin.getline(line_read, 100, ';');
+
+        if (line_read == nullptr) {
+            // EOF encountered
+            break;
+        }
+        std::string line = line_read;
+        line.append(";");
+        delete line_read;
+        //std::cout << "OK!INPUT IS: " << line << std::endl;
+
+        if (!line.empty()) {
+            YY_BUFFER_STATE buf = yy_scan_string(line.c_str());
+            if (yyparse() == 0) {
+                if (ast::parse_tree != nullptr) {
+                    ast::parse_tree.get()->debug_print();
+                }
+            }
+            yy_delete_buffer(buf);
+        }
+    }
+    return 0;
+}
